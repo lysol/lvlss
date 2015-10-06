f51f4bf05e5887685926888487a795a81fa07506
Derek Arnold <github@vyr.us>
2015-10-06 08:16:59 -0500

    starting to move to angular

---
+++ src/static/bower.js
@@ -0,0 +1,24 @@
+{
+  "name": "lvlss",
+  "version": "0.0.1",
+  "authors": [
+    "Derek Arnold <derek@derekarnold.net>"
+  ],
+  "description": "lvlss",
+  "main": "index.html",
+  "keywords": [
+    "twitter"
+  ],
+  "license": "MIT",
+  "homepage": "http://github.com/lysol/lvlss",
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ],
+  "dependencies": {
+    "angular": "~1.4.0"
+  }
+}
---
+++ src/static/js/lvlss.js
@@ -0,0 +1,3 @@
+angular.module('lvlss', [
+    'ui-router'
+]);
