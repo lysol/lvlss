ae7e8ddb26c9817da3da143ea42afded82f0813a
Derek Arnold <github@vyr.us>
2015-10-06 22:41:55 -0500

    travel works.

--- src/static/js/gameController.js
+++ src/static/js/gameController.js
@@ -1,10 +1,11 @@
-App.controller('lvlss.gameController', ['$scope', 'socket', '$location', function($scope, socket, $location) {
+var gameController = function($scope, socket, $location) {
     var self = this;
     this.inventory = [];
     this.location = {};
     this.location_areas = [];
     this.location_items = [];
     this.clientcrap = [];
+    this.socket = socket;
 
     $scope.$on('location', function(evt, data) {
         self.location = data.area;
@@ -22,4 +23,10 @@ App.controller('lvlss.gameController', ['$scope', 'socket', '$location', functio
         self.inventory = data.inventory;
     });
 
-}]);
+};
+
+gameController.prototype.travel = function(areaId) {
+    this.socket.emit('cmd', {command: 'go', args: [areaId]});
+};
+
+App.controller('lvlss.gameController', ['$scope', 'socket', '$location', gameController]);
